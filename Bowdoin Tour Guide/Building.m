c31c7a027d002daf8c5854c6b4ce4fd7c8cecdbd
Enrique Naudon <enaudon@bowdoin.edu>
2011-12-04 13:50:30 -0500

    fixed a small issue with the description method for the building class

--- Bowdoin Tour Guide/Building.m
+++ Bowdoin Tour Guide/Building.m
@@ -50,7 +50,11 @@
 
 - (NSString *)description
 {
-    return [NSString stringWithFormat:@"%@/n/t%@/n/t%g/n/t%g", self.title, self.subtitle, self.coordinate.latitude, self.coordinate.longitude];
+  //this is ugly, but \t (tab) isn't getting handled properly.  also, we don't
+  //want to use multiple lines if we can avoid it.
+  return [NSString stringWithFormat:@"%@_%@_(%g, %g)",
+          self.title, self.subtitle,
+          self.coordinate.latitude, self.coordinate.longitude];
 }
 
 @end
